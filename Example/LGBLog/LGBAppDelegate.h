3e6a531d1a58d8f4252bff1b0653ca6330c05c46
lgb789 <lgb789@126.com>
2016-03-24 13:12:32 +0800

    Initial commit

---
+++ Example/LGBLog/LGBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LGBAppDelegate.h
+//  LGBLog
+//
+//  Created by lgb789 on 03/24/2016.
+//  Copyright (c) 2016 lgb789. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LGBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LGBLog/LGBViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGBViewController.h
+//  LGBLog
+//
+//  Created by lgb789 on 03/24/2016.
+//  Copyright (c) 2016 lgb789. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LGBViewController : UIViewController
+
+@end
